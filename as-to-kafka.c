6da48cdfa01b273a4a57b66e031d1f6c7b418318
Jan Hecking <jhecking@netgaroo.com>
2017-04-26 17:34:08 +0800

    Formatting

--- as-to-kafka.c
+++ as-to-kafka.c
@@ -35,24 +35,25 @@
 
 #include "util.h"
 
-static int run = 1;
-
-static rd_kafka_t* rk;         /* Producer instance handle */
-static rd_kafka_topic_t* rkt;  /* Topic object */
+static rd_kafka_t* g_rk;         // Producer instance handle
+static rd_kafka_topic_t* g_rkt;  // Topic object
+static int g_run = 1;            // Run main loop while this is true
 
 /**
  * Signal termination of program
  */
-static void stop (int sig) {
-	run = 0;
+static void
+stop (int sig)
+{
+	g_run = 0;
 }
-
 
 
 /**
  * Packs a key into a Msgpack buffer
  */
-void pack_key(msgpack_sbuffer* sbuf, const as_key* key)
+void
+pack_key(msgpack_sbuffer* sbuf, const as_key* key)
 {
 	msgpack_sbuffer_init(sbuf);
 
@@ -76,7 +77,8 @@ void pack_key(msgpack_sbuffer* sbuf, const as_key* key)
 /**
  * Packs a single bin into a Msgpack buffer
  */
-bool pack_bin(const char* name, const as_val* val, void* data)
+bool
+pack_bin(const char* name, const as_val* val, void* data)
 {
 	msgpack_packer* pk = (msgpack_packer*) data;
 	size_t len = strlen(name);
@@ -85,7 +87,7 @@ bool pack_bin(const char* name, const as_val* val, void* data)
 
 	bool packed = false;
 	switch(as_val_type(val)) {
-    	case AS_STRING: {
+		case AS_STRING: {
 			as_string* str_val = as_string_fromval(val);
 			if (str_val) {
 				msgpack_pack_str(pk, str_val->len);
@@ -94,7 +96,7 @@ bool pack_bin(const char* name, const as_val* val, void* data)
 			}
 			break;
 		}
-    	case AS_BYTES: {
+		case AS_BYTES: {
 			as_bytes* bytes_val = as_bytes_fromval(val);
 			if (bytes_val) {
 				msgpack_pack_bin(pk, bytes_val->size);
@@ -111,7 +113,7 @@ bool pack_bin(const char* name, const as_val* val, void* data)
 			}
 			break;
 		}
-    	case AS_INTEGER: {
+		case AS_INTEGER: {
 			as_integer* int_val = as_integer_fromval(val);
 			if (int_val) {
 				msgpack_pack_int64(pk, int_val->value);
@@ -119,7 +121,7 @@ bool pack_bin(const char* name, const as_val* val, void* data)
 			}
 			break;
 		}
-    	case AS_GEOJSON: {
+		case AS_GEOJSON: {
 			as_geojson* geo_val = as_geojson_fromval(val);
 			if (geo_val) {
 				// TBD: pack as ext type instead?
@@ -130,16 +132,16 @@ bool pack_bin(const char* name, const as_val* val, void* data)
 			break;
 		}
 		// TODO
-    	case AS_LIST:
+		case AS_LIST:
 		// TODO
-    	case AS_MAP:
+		case AS_MAP:
 		// These value types should never appear in a record:
 		case AS_UNDEF:
-    	case AS_NIL:
-    	case AS_BOOLEAN:
-    	case AS_REC:
-    	case AS_PAIR:
-			 break;
+		case AS_NIL:
+		case AS_BOOLEAN:
+		case AS_REC:
+		case AS_PAIR:
+			break;
 	}
 	if (!packed) {
 		fprintf(stderr, "Skipping bin of type %u\n", as_val_type(val));
@@ -151,7 +153,8 @@ bool pack_bin(const char* name, const as_val* val, void* data)
 /**
  * Packs a record into a Msgpack buffer
  */
-void pack_record(msgpack_sbuffer* sbuf, const as_record* rec)
+void
+pack_record(msgpack_sbuffer* sbuf, const as_record* rec)
 {
 	msgpack_sbuffer_init(sbuf);
 
@@ -163,6 +166,7 @@ void pack_record(msgpack_sbuffer* sbuf, const as_record* rec)
 
 	as_record_foreach(rec, pack_bin, (void*) &pk);
 }
+
 /**
  * Message delivery report callback.
  *
@@ -174,8 +178,8 @@ void pack_record(msgpack_sbuffer* sbuf, const as_record* rec)
  * The callback is triggered from rd_kafka_poll() and executes on
  * the application's thread.
  */
-static void dr_msg_cb (rd_kafka_t *rk,
-		const rd_kafka_message_t *rkmessage, void *opaque)
+static void
+dr_msg_cb(rd_kafka_t *rk, const rd_kafka_message_t *rkmessage, void *opaque)
 {
 	if (rkmessage->err) {
 		fprintf(stderr, "%% Message delivery failed: %s\n",
@@ -187,10 +191,11 @@ static void dr_msg_cb (rd_kafka_t *rk,
 				rkmessage->len, rkmessage->partition);
 	}
 
-	/* The rkmessage is destroyed automatically by librdkafka */
+	// The rkmessage is destroyed automatically by librdkafka
 }
 
-static bool query_cb (const as_val* val, void* udata)
+static bool
+query_cb (const as_val* val, void* udata)
 {
 	if (!val) {
 		// query callback returned null - query is complete
@@ -200,7 +205,8 @@ static bool query_cb (const as_val* val, void* udata)
 	fprintf(stdout, "%% Processing updated record\n");
 	as_record* rec = as_record_fromval(val);
 	if (!rec) {
-		fprintf(stderr, "query callback return value of unexpected type %d\n", as_val_type(val));
+		fprintf(stderr, "query callback return value of unexpected type %d\n",
+				as_val_type(val));
 		return true;
 	}
 
@@ -214,59 +220,24 @@ static bool query_cb (const as_val* val, void* udata)
 	hexdump(stdout, "record", rec_sbuf.data, rec_sbuf.size);
 	unpack(stdout, "record", rec_sbuf.data, rec_sbuf.size);
 
-	/*
-	 * Send/Produce message.
-	 * This is an asynchronous call, on success it will only
-	 * enqueue the message on the internal producer queue.
-	 * The actual delivery attempts to the broker are handled
-	 * by background threads.
-	 * The previously registered delivery report callback
-	 * (dr_msg_cb) is used to signal back to the application
-	 * when the message has been delivered (or failed).
-	 */
 retry:
-	if (rd_kafka_produce(
-				/* Topic object */
-				rkt,
-				/* Use builtin partitioner to select partition*/
-				RD_KAFKA_PARTITION_UA,
-				/* Make a copy of the payload. */
-				RD_KAFKA_MSG_F_COPY,
-				/* Message payload (value) and length */
-				rec_sbuf.data, rec_sbuf.size,
-				/* Optional key and its length */
-				key_sbuf.data, key_sbuf.size,
-				/* Message opaque, provided in
-				 * delivery report callback as
-				 * msg_opaque. */
+	if (rd_kafka_produce(g_rkt, RD_KAFKA_PARTITION_UA, RD_KAFKA_MSG_F_COPY,
+				rec_sbuf.data, rec_sbuf.size, key_sbuf.data, key_sbuf.size,
 				NULL) == -1) {
-		/**
-		 * Failed to *enqueue* message for producing.
-		 */
 		fprintf(stderr,
 				"%% Failed to produce to topic %s: %s\n",
-				rd_kafka_topic_name(rkt),
+				rd_kafka_topic_name(g_rkt),
 				rd_kafka_err2str(rd_kafka_last_error()));
 
-		/* Poll to handle delivery reports */
-		if (rd_kafka_last_error() ==
-				RD_KAFKA_RESP_ERR__QUEUE_FULL) {
-			/* If the internal queue is full, wait for
-			 * messages to be delivered and then retry.
-			 * The internal queue represents both
-			 * messages to be sent and messages that have
-			 * been sent or failed, awaiting their
-			 * delivery report callback to be called.
-			 *
-			 * The internal queue is limited by the
-			 * configuration property
-			 * queue.buffering.max.messages */
-			rd_kafka_poll(rk, 1000/*block for max 1000ms*/);
+		if (rd_kafka_last_error() == RD_KAFKA_RESP_ERR__QUEUE_FULL) {
+			// If the internal queue is full, wait for messages to be delivered
+			// and then retry.
+			rd_kafka_poll(g_rk, 1000);
 			goto retry;
 		}
 	} else {
 		fprintf(stderr, "Enqueued message (%zu bytes) for topic %s\n",
-				rec_sbuf.size, rd_kafka_topic_name(rkt));
+				rec_sbuf.size, rd_kafka_topic_name(g_rkt));
 	}
 
 	msgpack_sbuffer_destroy(&key_sbuf);
@@ -275,7 +246,8 @@ retry:
 	return true;
 }
 
-void run_query(aerospike* as, const char* namespace, const char* set, const time_t* time)
+void
+run_query(aerospike* as, const char* namespace, const char* set, const time_t* time)
 {
 	as_error err;
 	as_query query;
@@ -290,8 +262,7 @@ void run_query(aerospike* as, const char* namespace, const char* set, const time
 	if (aerospike_query_foreach(as, &err, NULL, &query, query_cb, NULL) !=
 			AEROSPIKE_OK) {
 		if (err.code != AEROSPIKE_ERR_RECORD_NOT_FOUND) {
-			fprintf(stderr,
-					"%% Failed to query Aerospike cluster: %s [%d]\n",
+			fprintf(stderr, "%% Failed to query Aerospike cluster: %s [%d]\n",
 					err.message, err.code);
 		}
 	}
@@ -299,38 +270,39 @@ void run_query(aerospike* as, const char* namespace, const char* set, const time
 	as_query_destroy(&query);
 }
 
-int main (int argc, char **argv)
+int
+main(int argc, char **argv)
 {
-	const char* brokers;    /* Argument: broker list */
-	const char* topic;      /* Argument: topic to produce to */
-	const char* hosts;      /* Argument: Aerospike cluster hosts */
-	const char* namespace;  /* Argument: Aerospike namespace */
-	const char* set;        /* Argument: Aerospike set */
-	rd_kafka_conf_t* conf;  /* Temporary configuration object */
-	char errstr[512];       /* librdkafka API error reporting buffer */
-	aerospike as;           /* Aerospike client */
-	as_config as_conf;      /* Temporary configuration object */
-	as_error as_err;        /* Aerospike client error object */
-	time_t last_query_ts;   /* Time of last query */
+	const char* brokers;    // Argument: broker list
+	const char* topic;      // Argument: topic to produce to
+	const char* hosts;      // Argument: Aerospike cluster hosts
+	const char* namespace;  // Argument: Aerospike namespace
+	const char* set;        // Argument: Aerospike set
+	rd_kafka_conf_t* conf;  // Temporary configuration object
+	char errstr[512];       // librdkafka API error reporting buffer
+	aerospike as;           // Aerospike client
+	as_config as_conf;      // Temporary configuration object
+	as_error as_err;        // Aerospike client error object
+	time_t last_query_ts;   // Time of last query
 
 	if (argc != 6) {
 		fprintf(stderr, "%% Usage: %s <broker> <topic> <hosts> <namespace> <set>\n", argv[0]);
 		return 1;
 	}
 
-	/* Kafka bootstrap broker(s) as a comma-separated list of
-	 * host or host:port (default port 9092).
-	 * librdkafka will use the bootstrap brokers to acquire the full
-	 * set of brokers from the cluster. */
+	// Kafka bootstrap broker(s) as a comma-separated list of
+	// host or host:port (default port 9092).
+	// librdkafka will use the bootstrap brokers to acquire the full
+	// set of brokers from the cluster.
 	brokers = argv[1];
-	/* Kafka topic to publish data to */
+	// Kafka topic to publish data to
 	topic = argv[2];
-	/* Aerospike cluster host(s) as a comma-separated list of
-	 * host or host:port (default port 3000). */
+	// Aerospike cluster host(s) as a comma-separated list of
+	// host or host:port (default port 3000).
 	hosts = argv[3];
-	/* Aerospike namespace to fetch data from */
+	// Aerospike namespace to fetch data from
 	namespace = argv[4];
-	/* Aerospike set to fetch data from */
+	// Aerospike set to fetch data from
 	set = argv[5];
 
 	conf = rd_kafka_conf_new();
@@ -341,17 +313,17 @@ int main (int argc, char **argv)
 	}
 	rd_kafka_conf_set_dr_msg_cb(conf, dr_msg_cb);
 
-	rk = rd_kafka_new(RD_KAFKA_PRODUCER, conf, errstr, sizeof(errstr));
-	if (!rk) {
+	g_rk = rd_kafka_new(RD_KAFKA_PRODUCER, conf, errstr, sizeof(errstr));
+	if (!g_rk) {
 		fprintf(stderr, "Failed to create new producer: %s\n", errstr);
 		return 1;
 	}
 
-	rkt = rd_kafka_topic_new(rk, topic, NULL);
-	if (!rkt) {
+	g_rkt = rd_kafka_topic_new(g_rk, topic, NULL);
+	if (!g_rkt) {
 		fprintf(stderr, "%% Failed to create topic object: %s\n",
 				rd_kafka_err2str(rd_kafka_last_error()));
-		rd_kafka_destroy(rk);
+		rd_kafka_destroy(g_rk);
 		return 1;
 	}
 
@@ -367,7 +339,7 @@ int main (int argc, char **argv)
 		fprintf(stderr, "%% Failed to connect to Aerospike cluster: %s [%d]\n",
 				as_err.message, as_err.code);
 		aerospike_destroy(&as);
-		rd_kafka_destroy(rk);
+		rd_kafka_destroy(g_rk);
 		return 1;
 	}
 
@@ -378,10 +350,10 @@ int main (int argc, char **argv)
 
 	time(&last_query_ts);
 
-	while (run) {
+	while (g_run) {
 		run_query(&as, namespace, set, &last_query_ts);
 		time(&last_query_ts);
-		rd_kafka_poll(rk, 0);
+		rd_kafka_poll(g_rk, 0);
 		sleep(5);
 	}
 
@@ -389,9 +361,9 @@ int main (int argc, char **argv)
 	aerospike_destroy(&as);
 
 	fprintf(stderr, "Flushing final messages..\n");
-	rd_kafka_flush(rk, 10 * 1000);
-	rd_kafka_topic_destroy(rkt);
-	rd_kafka_destroy(rk);
+	rd_kafka_flush(g_rk, 10 * 1000);
+	rd_kafka_topic_destroy(g_rkt);
+	rd_kafka_destroy(g_rk);
 
 	return 0;
 }
